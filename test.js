11c2ed96ff34219ffa4b869ecf6cc7692bc7971d
anic4cio <anicaciovinicius36@gmail.com>
2022-06-17 13:57:21 -0300

    [update] - add test

--- test.js
+++ test.js
@@ -1,8 +1,15 @@
-// var test = require('express');
-var test = require('socket.io');
+var express = require('express');
+var socket = require('socket.io');
 
-if(test) {
-    console.log(`It's there.`);
+if(express) {
+    console.log(`It's there. ${express}`);
 } else {
-    console.log(`It isn't there`);
+    console.log(`It isn't there. ${express}`);
+}
+
+
+if(socket) {
+    console.log(`It's there. ${socket}`);
+} else {
+    console.log(`It isn't there. ${socket}`);
 }
